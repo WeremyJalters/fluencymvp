a0af92bbe720b8802ea1bdddef91c962e381c777
Jeremy Walters <jeremyjwalters@gmail.com>
2014-09-09 19:47:36 -0400

    forum up and functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+def forem_name
+    username
 end
+
+def forem_email
+  email_address
+end
+
+	
+end
+
